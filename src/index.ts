6c2f3aa6bd11d5b6e1c17f5a73bb1db482695442
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-12 15:51:21 +0200

    feat: add vite

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { createNode, updateElement } from './core/runtime';
+const item1 = createNode('li', {}, ['item 1']);
+const item2 = createNode('li', {}, ['item 1']);
+const me = createNode(
+  'img',
+  {
+    src: 'https://github.com/yazaldefilimonepinto.png',
+    style: {
+      width: '200px',
+      height: '200px',
+      borderRadius: '50%',
+    },
+  },
+  [],
+);
+const tk = createNode(
+  'img',
+  {
+    src: 'https://github.com/imteekay.png',
+    style: {
+      width: '200px',
+      height: '200px',
+      borderRadius: '50%',
+    },
+  },
+  [],
+);
+
+const ComponentME = createNode('ul', { class: 'list' }, [item1, item2, me]);
+const ComponentTK = createNode('ul', { class: 'list' }, [item1, item2, tk]);
+
+const $root = document.getElementById('app');
+const $reload = document.getElementById('reload');
+
+$root && updateElement($root, 0, ComponentME);
+$reload?.addEventListener('click', () => {
+  if ($root) updateElement($root, 0, ComponentTK, ComponentME);
+});
